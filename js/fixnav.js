d6e41e4c0928dee4b387e61237236c2c5d837b43
Celia Hsu <celiahsu@eztravel.com.tw>
2022-06-09 17:40:26 +0800

    上傳靜態網站

---
+++ js/fixnav.js
@@ -0,0 +1,12 @@
+$(function () {
+    let scrollTop = 0;
+    let navHi = $('nav').outerHeight();
+    $(window).scroll(function () {
+        scrollTop = $(window).scrollTop();
+        if (scrollTop >= navHi / 2) {
+            $('.page_header').addClass('fixed');
+        } else {
+            $('.page_header').removeClass('fixed');
+        }
+    });
+});
